b3a7b3af8d074e38d8ada62e1a50b0b4da870c58
Oscar Andres Ochoa Villegas <ochoscar@gmail.com>
2026-08-11 14:43:27 -0500

    Elixir recursion

--- lib/playground/algorithms.ex
+++ lib/playground/algorithms.ex
@@ -35,4 +35,61 @@ defmodule Playground.Algorithms do
   def span(from, to, span_list) when from < to, do: span(from + 1, to, span_list ++ [ from + 1])
   def span(from, to, span_list) when from === to, do: span_list
 
+  # ---- own all?
+  def all?([ head | tail ], fun), do: fun.(head) and all?(tail, fun)
+  def all?([], _fun), do: true
+
+  # ---- own each
+  def each([ head | tail ], func) do
+    func.(head)
+    each(tail, func)
+  end
+  def each([], _func), do: :ok
+
+  # ---- own filter
+  def filter([ head | tail ], func) do
+    if func.(head) do
+      [ head | filter(tail, func) ]
+    else
+      filter(tail, func)
+    end
+  end
+  def filter([], _func), do: []
+
+  # ---- own split
+  def split(list, n), do: split(list, n, 0, [], [])
+  def split([ head | tail ], n, acc, left, right) do
+    cond do
+      n > acc ->
+        split(tail, n, acc + 1, [head | left], right)
+
+      n <= acc ->
+        split([], n, acc + 1, left, [ head | tail ])
+    end
+  end
+  def split([], _n, _acc, left, right), do: [Enum.reverse(left), right]
+
+  # ---- own take
+  def take(list, n), do: take(list, n, 0, [])
+  def take([ head | tail ], n, acc, list_n) when acc < n do
+     take(tail, n, acc + 1, [ head | list_n ])
+  end
+  def take([ _head | _tail ], n, acc, list_n) when acc == n do
+     Enum.reverse list_n
+  end
+
+  # ---- own flatten flatten([1, [ 2, 3, [ 4 ] ], 5, [[[6]]] ]) = [1,2,3,4,5,6]
+  def flatten(list), do: Enum.reverse(flatten(list, []))
+  def flatten([[] | tail], flist), do: flatten(tail, flist)
+  def flatten([ head | tail ], flist) when is_list(head) do
+    [ hh | ht ] = head
+    hhflist = flatten([ hh ], flist)
+    htflist = flatten(ht, hhflist)
+    flatten(tail, htflist)
+  end
+  def flatten([ head | tail ], flist) when not is_list(head) do
+    flatten(tail, [head | flist])
+  end
+  def flatten([], flist), do: flist
+
 end
